54b814f55f9485b552754e98e9243cb918aa0de9
Selva Ganesh M <selvadev2k@gmail.com>
2023-02-21 00:44:00 +0530

    posts model and inserted many posts

---
+++ src/models/post.model.ts
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+
+export type TPost = {
+  cover: string;
+  title: string;
+  category: string;
+  desc: string;
+  likes?: Array<string>;
+};
+
+const postSchema = new mongoose.Schema<TPost>({
+  cover: {
+    type: String,
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  desc: {
+    type: String,
+    required: true,
+  },
+  likes: {
+    type: [{ type: String }],
+    default: [],
+  },
+});
+
+const postModel = mongoose.model("Post", postSchema);
+
+export default postModel;
