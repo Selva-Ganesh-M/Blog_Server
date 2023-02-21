1c80426480097f2ed6a95d60ec89d63d784642ac
Selva Ganesh M <selvadev2k@gmail.com>
2023-02-21 18:20:36 +0530

    like dislike functionality

--- src/controllers/posts.ctrl.ts
+++ src/controllers/posts.ctrl.ts
@@ -1,5 +1,7 @@
 import { Request, Response } from "express";
 import postModel from "../models/post.model";
+import asyncHandler from "express-async-handler";
+import { customError } from "../utils/customError";
 
 export const getAllPosts = async (req: Request, res: Response) => {
   const posts = await postModel.find();
@@ -10,3 +12,88 @@ export const getAllPosts = async (req: Request, res: Response) => {
     payload: posts,
   });
 };
+
+// like post
+export const likePost = asyncHandler(
+  async (
+    req: Request<{ id: string }, {}, { userId: string }>,
+    res: Response
+  ) => {
+    console.log("like post");
+
+    // update the /model
+    const post = await postModel.findByIdAndUpdate(
+      {
+        _id: req.params.id,
+      },
+      {
+        $addToSet: {
+          likes: req.body.userId,
+        },
+      },
+      {
+        new: true,
+      }
+    );
+
+    // response
+    res.status(200).json({
+      statusText: "success",
+      statusCode: 200,
+      message: "like success",
+      payload: post,
+    });
+  }
+);
+
+// dis-like post
+export const dislikePost = asyncHandler(
+  async (
+    req: Request<{ id: string }, {}, { userId: string }>,
+    res: Response
+  ) => {
+    console.log("dislike post");
+
+    // update the /model
+    const post = await postModel.findByIdAndUpdate(
+      {
+        _id: req.params.id,
+      },
+      {
+        $pull: {
+          likes: req.body.userId,
+        },
+      },
+      {
+        new: true,
+      }
+    );
+
+    // response
+    res.status(200).json({
+      statusText: "success",
+      statusCode: 200,
+      message: "like success",
+      payload: post,
+    });
+  }
+);
+
+// get a post
+export const getsinglePost = asyncHandler(
+  async (req: Request<{ id: string }>, res: Response) => {
+    console.log("get single post");
+
+    const post = await postModel.findById(req.params.id);
+    if (!post)
+      throw new customError(404, "fetch post failed: requested post not found");
+
+    // response
+    res.status(200).json({
+      statusText: "success",
+      statusCode: 200,
+      message: "post found",
+      payload: post,
+    });
+  }
+);
--- src/routers/post.route.ts
+++ src/routers/post.route.ts
@@ -1,7 +1,21 @@
 import express from "express";
-import { getAllPosts } from "../controllers/posts.ctrl";
+import {
+  dislikePost,
+  getAllPosts,
+  getsinglePost,
+  likePost,
+} from "../controllers/posts.ctrl";
 
 const postsRouter = express.Router();
+
+// like a post
+postsRouter.post("/like/:id", likePost);
+
+// dislike a post
+postsRouter.post("/dislike/:id", dislikePost);
+
+// get single post
+postsRouter.get("/:id", getsinglePost);
 
 // get all posts
 postsRouter.get("/", getAllPosts);
